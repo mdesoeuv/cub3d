8fb5ea4cef7bf666f4564d614d0748d610c33885
Victor Chevillotte <victor.chevillotte@gmail.com>
2022-02-23 13:53:41 +0100

    differnciation des 4 directions OK

--- srcs/raycaster_3d.c
+++ srcs/raycaster_3d.c
@@ -6,7 +6,7 @@
 /*   By: vchevill <vchevill@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 10:13:32 by mdesoeuv          #+#    #+#             */
-/*   Updated: 2022/02/23 13:42:14 by vchevill         ###   ########lyon.fr   */
+/*   Updated: 2022/02/23 13:53:12 by vchevill         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,12 +59,14 @@ void	draw_rays_3d(t_cub3d *cub3d)
 			put_pixel_to_image_3d(cub3d, column, y, create_trgb(0, cub3d->color_ceiling[0], cub3d->color_ceiling[1], cub3d->color_ceiling[2]));
 		while (y < wall_size / 2 + offset)
 		{
-			if (cub3d->map.ray_hit_x == 1  && angle_diff > M_PI / 2 && angle_diff < (3 * M_PI)/2)
+			if (cub3d->map.ray_hit_x == 1  && angle_tmp > M_PI / 2 && angle_tmp < (3 * M_PI)/2)
 				put_pixel_to_image_3d(cub3d, column, y, create_trgb(0, 0, 0, 150));
 			else if (cub3d->map.ray_hit_x == 1)
 				put_pixel_to_image_3d(cub3d, column, y, create_trgb(0, 0, 150, 0));
-			else
+			else if (cub3d->map.ray_hit_x == 0  && angle_tmp > 0 && angle_tmp < M_PI)
 				put_pixel_to_image_3d(cub3d, column, y, create_trgb(0, 150, 0, 0));
+			else
+				put_pixel_to_image_3d(cub3d, column, y, create_trgb(0, 150, 150, 0));
 				y++;
 		}
 		while (y < WINDOW_HEIGHT)
