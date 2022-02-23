d5840999a958734fd92b6dc4d169a23dc322c1a2
Victor Chevillotte <victor.chevillotte@gmail.com>
2022-02-23 17:06:32 +0100

    working

--- srcs/raycaster_3d.c
+++ srcs/raycaster_3d.c
@@ -6,7 +6,7 @@
 /*   By: vchevill <vchevill@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 10:13:32 by mdesoeuv          #+#    #+#             */
-/*   Updated: 2022/02/23 16:58:53 by vchevill         ###   ########lyon.fr   */
+/*   Updated: 2022/02/23 17:06:19 by vchevill         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,12 +73,12 @@ void	draw_rays_3d(t_cub3d *cub3d)
 			}
 			else if (cub3d->map.ray_hit_x == 0  && angle_tmp > 0 && angle_tmp < M_PI)
 			{
-				ft_pixel_texture_color(cub3d, cub3d->texture_n, x, y);
+				ft_pixel_texture_color(cub3d, cub3d->texture_s, x, y);
 				put_pixel_to_image_3d(cub3d, column, y, create_trgb(0, cub3d->color_texture[0], cub3d->color_texture[1], cub3d->color_texture[2]));
 			}
 			else
 			{
-				ft_pixel_texture_color(cub3d, cub3d->texture_n, x, y);
+				ft_pixel_texture_color(cub3d, cub3d->texture_w, x, y);
 				put_pixel_to_image_3d(cub3d, column, y, create_trgb(0, cub3d->color_texture[0], cub3d->color_texture[1], cub3d->color_texture[2]));
 			}
 			y++;
