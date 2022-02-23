ba7cf7b103d58b2604961675cd26cad4c381a3b0
Mehdi <mehdi.desoeuvre@gmail.com>
2022-02-23 10:09:14 +0100

    d

--- srcs/raycaster.c
+++ srcs/raycaster.c
@@ -6,7 +6,7 @@
 /*   By: mdesoeuv <mdesoeuv@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 09:13:42 by mdesoeuv          #+#    #+#             */
-/*   Updated: 2022/02/23 09:59:39 by mdesoeuv         ###   ########lyon.fr   */
+/*   Updated: 2022/02/23 10:08:03 by mdesoeuv         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,31 +60,33 @@ double	ft_ray_length(t_cub3d *cub3d, double angle)
 	double		v_dx_length;
 	int			interesec_to_pass_x;
 	int			interesec_to_pass_y;
+	int			is_x_updated;
 
 	interesec_to_pass_x = 0;
 	interesec_to_pass_y = 0;
+	is_x_updated = -1;
 	
 	dprintf(2, "player angle %f\n", cub3d->player_angle);
 	while (cub3d->map.map[(int)(cub3d->player.y / CUBE_SIZE) + interesec_to_pass_y][(int)(cub3d->player.x / CUBE_SIZE) + interesec_to_pass_x] != '1')
 	{
 		//dprintf(1, "y_to_check=%i\n", (int)(cub3d->player.y / CUBE_SIZE) + interesec_to_pass_y);
 		//dprintf(1, "x_to_check=%i\n", (int)(cub3d->player.x / CUBE_SIZE) + interesec_to_pass_x);
-		if ( angle > 0 && angle < M_PI) // vecteur vers le haut (y positif)
+		if ( angle > 0 && angle < M_PI && is_x_updated != 1) // vecteur vers le haut (y positif)
 		{
 			v_dy_length = fabs((CUBE_SIZE - (cub3d->player.y % CUBE_SIZE) + (CUBE_SIZE * abs(interesec_to_pass_y))) / sin(angle));
 			// dprintf(1, "dy=%d, angle=%f\n", (CUBE_SIZE - (cub3d->player.y % CUBE_SIZE) + (CUBE_SIZE * abs(interesec_to_pass_y))), angle);
 		}
-		else // vecteur vers le haut (y negatif)
+		else if (is_x_updated != 1)// vecteur vers le haut (y negatif)
 		{
 			v_dy_length = fabs(((cub3d->player.y % CUBE_SIZE) + (CUBE_SIZE * abs(interesec_to_pass_y))) / sin(angle));
 			// dprintf(1, "dy=%d, angle=%f\n", (cub3d->player.y % CUBE_SIZE) + (CUBE_SIZE * abs(interesec_to_pass_y)), angle);
 		}
-		if ( angle > M_PI / 2 && angle < (3 * M_PI)/2) // vecteur vers le bas ?? (y positif ?)
+		if ( angle > M_PI / 2 && angle < (3 * M_PI)/2 && is_x_updated != 0) // vecteur vers le bas ?? (y positif ?)
 		{
 			v_dx_length = fabs(((cub3d->player.x % CUBE_SIZE) + (CUBE_SIZE * abs(interesec_to_pass_x))) / cos(angle));
 			// dprintf(1, "dx=%d, angle=%f\n", ((cub3d->player.x % CUBE_SIZE) + (CUBE_SIZE * abs(interesec_to_pass_x))), angle);
 		}
-		else // vecteur vers le haut (y negatif)
+		else if (is_x_updated != 0) // vecteur vers le haut (y negatif)
 		{
 			v_dx_length = fabs((CUBE_SIZE - (cub3d->player.x % CUBE_SIZE) + (CUBE_SIZE * abs(interesec_to_pass_x))) / cos(angle));
 			// dprintf(1, "dx=%d, angle=%f\n", (CUBE_SIZE - (cub3d->player.x % CUBE_SIZE) + (CUBE_SIZE * abs(interesec_to_pass_x))), angle);
@@ -93,13 +95,25 @@ double	ft_ray_length(t_cub3d *cub3d, double angle)
 		// dprintf(1, "v_dx_length=%f\n", v_dx_length);
 
 		if (v_dy_length > v_dx_length && !( angle > M_PI / 2 && angle < (3 * M_PI)/2)) // si longueur du vecteur qui s'arrete aux x < a celui qui s'arrete aux y
+		{
+			is_x_updated = 1;
 			interesec_to_pass_x++;
+		}
 		else if (v_dy_length > v_dx_length)
+		{
+			is_x_updated = 1;
 			interesec_to_pass_x--;
+		}
 		else if (v_dy_length < v_dx_length && !( angle > 0 && angle < M_PI))
+		{
+			is_x_updated = 0;
 			interesec_to_pass_y--;
+		}
 		else 
+		{
+			is_x_updated = 0;
 			interesec_to_pass_y++;
+		}
 	}
 	// dprintf(1, "y_to_check=%i\n", (int)(cub3d->player.y / CUBE_SIZE) + interesec_to_pass_y);
 	// dprintf(1, "x_to_check=%i\n", (int)(cub3d->player.x / CUBE_SIZE) + interesec_to_pass_x);
@@ -118,10 +132,10 @@ void	draw_rays(t_cub3d *cub3d)
 	int		i;
 	double	angle_tmp;
 
-	i = -10;
-	while ( i < 10)
+	i = -500;
+	while ( i < 500)
 	{
-		angle_tmp = cub3d->player_angle + M_PI / 40 * i;
+		angle_tmp = cub3d->player_angle + M_PI / 1980 * i;
 		if (angle_tmp > 2 * M_PI)
 			angle_tmp -= 2 * M_PI;
 		ray_length = ft_ray_length(cub3d, angle_tmp);
