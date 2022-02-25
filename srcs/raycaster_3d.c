f02c6d5bf8c5c5601f6567e3554719e16dc89277
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2022-02-25 10:39:42 +0100

    del offset x

--- srcs/raycaster_3d.c
+++ srcs/raycaster_3d.c
@@ -6,7 +6,7 @@
 /*   By: vchevill <vchevill@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 10:13:32 by mdesoeuv          #+#    #+#             */
-/*   Updated: 2022/02/25 10:36:39 by vchevill         ###   ########.fr       */
+/*   Updated: 2022/02/25 10:39:25 by vchevill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,21 +48,17 @@ int static		ft_fish_eye_correction(double player_angle, double ray_abs_angle, do
 
 void	draw_rays_3d(t_cub3d *cub3d)
 {
-	int		column;
 	double	ray_length;
 	int		x;
 	double	ray_abs_angle;
 	double	wall_size;
 	int		y;
 	int		offset_y;
-	int		offset_x;
-
 	double	ratio;
 	int		y_wall;
 	int		x_wall;
 
 	offset_y = WINDOW_HEIGHT / 2;
-	offset_x = WINDOW_WIDTH / 2;
 	x = 0;
 	x_wall = -1;
 	ray_abs_angle = (cub3d->player_angle - (cub3d->fov / 2)) - (cub3d->fov / WINDOW_WIDTH);
@@ -76,10 +72,9 @@ void	draw_rays_3d(t_cub3d *cub3d)
 		wall_size = cub3d->slice_ratio / ray_length;
 		if (wall_size > WINDOW_HEIGHT)//a retirer apres
 			wall_size = WINDOW_HEIGHT;
-		column = (x - offset_x) + (WINDOW_WIDTH / 2);
 		y = -1;
 		while (++y < offset_y - wall_size / 2)
-			put_pixel_to_image_3d(cub3d, column, y, create_trgb(0, cub3d->color_ceiling[0], cub3d->color_ceiling[1], cub3d->color_ceiling[2]));
+			put_pixel_to_image_3d(cub3d, x, y, create_trgb(0, cub3d->color_ceiling[0], cub3d->color_ceiling[1], cub3d->color_ceiling[2]));
 		y_wall = -1;
 		while (y < wall_size / 2 + offset_y)
 		{
@@ -88,31 +83,31 @@ void	draw_rays_3d(t_cub3d *cub3d)
 			{
 				ratio = wall_size / cub3d->texture_e.height;
 				ft_pixel_texture_color(cub3d, cub3d->texture_e, ft_texture_x_offset(cub3d, ray_abs_angle, ray_length) / ratio, y_wall / ratio);
-				put_pixel_to_image_3d(cub3d, column, y, create_trgb(0, cub3d->color_texture[0], cub3d->color_texture[1], cub3d->color_texture[2]));
+				put_pixel_to_image_3d(cub3d, x, y, create_trgb(0, cub3d->color_texture[0], cub3d->color_texture[1], cub3d->color_texture[2]));
 			}
 			else if (cub3d->map.ray_has_hit_x == 0)
 			{
 				ratio = wall_size / cub3d->texture_w.height;
 				dprintf(1, "%d\n", ((int)(fabs(ray_length * sin(ray_abs_angle)) - (CUBE_SIZE - (cub3d->player.x % CUBE_SIZE)) % CUBE_SIZE)));
 				ft_pixel_texture_color(cub3d, cub3d->texture_w,  ft_texture_x_offset(cub3d, ray_abs_angle, ray_length) / ratio, y_wall / ratio);
-				put_pixel_to_image_3d(cub3d, column, y, create_trgb(0, cub3d->color_texture[0], cub3d->color_texture[1], cub3d->color_texture[2]));
+				put_pixel_to_image_3d(cub3d, x, y, create_trgb(0, cub3d->color_texture[0], cub3d->color_texture[1], cub3d->color_texture[2]));
 			}
 			else if (cub3d->map.ray_has_hit_x == 1  && ray_abs_angle > 0 && ray_abs_angle < M_PI)
 			{
 				ratio = wall_size / cub3d->texture_n.height;
 				ft_pixel_texture_color(cub3d, cub3d->texture_n,  ft_texture_x_offset(cub3d, ray_abs_angle, ray_length) / ratio, y_wall / ratio);
-				put_pixel_to_image_3d(cub3d, column, y, create_trgb(0, cub3d->color_texture[0], cub3d->color_texture[1], cub3d->color_texture[2]));
+				put_pixel_to_image_3d(cub3d, x, y, create_trgb(0, cub3d->color_texture[0], cub3d->color_texture[1], cub3d->color_texture[2]));
 			}
 			else
 			{
 				ratio = wall_size / cub3d->texture_s.height;
 				ft_pixel_texture_color(cub3d, cub3d->texture_s,  ft_texture_x_offset(cub3d, ray_abs_angle, ray_length) / ratio, y_wall / ratio);
-				put_pixel_to_image_3d(cub3d, column, y, create_trgb(0, cub3d->color_texture[0], cub3d->color_texture[1], cub3d->color_texture[2]));
+				put_pixel_to_image_3d(cub3d, x, y, create_trgb(0, cub3d->color_texture[0], cub3d->color_texture[1], cub3d->color_texture[2]));
 			}
 			y++;
 		}
 		while (y < WINDOW_HEIGHT)
-			put_pixel_to_image_3d(cub3d, column, y++, create_trgb(0, cub3d->color_ground[0], cub3d->color_ground[1], cub3d->color_ground[2]));
+			put_pixel_to_image_3d(cub3d, x, y++, create_trgb(0, cub3d->color_ground[0], cub3d->color_ground[1], cub3d->color_ground[2]));
 		x++;
 	}
 }
