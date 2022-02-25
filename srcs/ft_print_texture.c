f6f76f4f163c6e022bb0e251cc47b7131f695015
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2022-02-25 16:06:12 +0100

    s

--- srcs/ft_print_texture.c
+++ srcs/ft_print_texture.c
@@ -6,7 +6,7 @@
 /*   By: vchevill <vchevill@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 16:05:14 by vchevill          #+#    #+#             */
-/*   Updated: 2022/02/25 16:04:30 by vchevill         ###   ########.fr       */
+/*   Updated: 2022/02/25 16:05:45 by vchevill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void	ft_pixel_texture_color(t_cub3d *cub3d, t_image image, int x, int y)
 {
-	cub3d->color_texture[2] = image.data[y * image.line_size + \
-		x * image.bits_per_pixel / 8];
-	cub3d->color_texture[1] = image.data[(y * image.line_size + \
-		x * image.bits_per_pixel / 8) + 1];
 	cub3d->color_texture[0] = image.data[(y * image.line_size + \
 		x * image.bits_per_pixel / 8) + 2];
+	cub3d->color_texture[1] = image.data[(y * image.line_size + \
+		x * image.bits_per_pixel / 8) + 1];
+	cub3d->color_texture[2] = image.data[y * image.line_size + \
+		x * image.bits_per_pixel / 8];
 }
 
 void	ft_init_texture(t_cub3d *cub3d, char *texture_path, t_image *image_struc)
