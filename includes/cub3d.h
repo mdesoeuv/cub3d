c6d84cc2994874e387b495c2c9499a6c24bd59e1
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2022-02-28 15:05:05 +0100

    s

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: vchevill <vchevill@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 09:54:12 by vchevill          #+#    #+#             */
-/*   Updated: 2022/02/28 15:02:04 by vchevill         ###   ########.fr       */
+/*   Updated: 2022/02/28 15:03:52 by vchevill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ corrupted or inaccessible.\n"
 
 # define CUBE_SIZE 258
 # define MOVE_SIZE 40
-# define ANGLE_INC 10
+# define ANGLE_INC 20
 # define FOV 45
 # define UP 13
 # define DOWN 1
