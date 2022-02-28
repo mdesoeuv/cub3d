8d1fce7adf744cce8960244527f932526474d9d1
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2022-02-28 15:23:20 +0100

    norme calc utils

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: vchevill <vchevill@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 09:54:12 by vchevill          #+#    #+#             */
-/*   Updated: 2022/02/28 15:03:52 by vchevill         ###   ########.fr       */
+/*   Updated: 2022/02/28 15:06:02 by vchevill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,7 @@ path has not been declared.\n"
 corrupted or inaccessible.\n"
 
 # define CUBE_SIZE 258
-# define MOVE_SIZE 40
+# define MOVE_SIZE 60
 # define ANGLE_INC 20
 # define FOV 45
 # define UP 13
--- srcs/calc_utils.c
+++ srcs/calc_utils.c
@@ -6,7 +6,7 @@
 /*   By: vchevill <vchevill@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 10:21:43 by mdesoeuv          #+#    #+#             */
-/*   Updated: 2022/02/25 15:51:13 by vchevill         ###   ########.fr       */
+/*   Updated: 2022/02/28 15:22:57 by vchevill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,62 +15,77 @@
 
 int	ft_particular_angle(int player_x, double ray_abs_angle)
 {
-	if (ray_abs_angle == M_PI / 2 || ray_abs_angle == 3 * M_PI /2)
+	if (ray_abs_angle == M_PI / 2 || ray_abs_angle == 3 * M_PI / 2)
 		return (player_x % CUBE_SIZE);
 	else
 		return (player_x % CUBE_SIZE);
+}
+
+double static	ft_calc_alpha_x(double ray_abs_angle)
+{
+	if (ray_abs_angle > 0 && ray_abs_angle < (M_PI / 2))
+		return (ray_abs_angle);
+	else if (ray_abs_angle > (3 * M_PI / 2) && ray_abs_angle < 2 * M_PI)
+		return (2 * M_PI - ray_abs_angle);
+	else if (ray_abs_angle > M_PI / 2 && ray_abs_angle < M_PI)
+		return ( M_PI - ray_abs_angle);
+	else if (ray_abs_angle > M_PI && ray_abs_angle < 3 * M_PI / 2)
+		return (ray_abs_angle - M_PI);
+	return (ray_abs_angle);
 }
 
 int	ft_texture_x_offset(t_cub3d *cub3d, double ray_abs_angle, double ray_length)
 {
 	double	alpha;
+
+	alpha = ft_calc_alpha_x(ray_abs_angle);
 	if (ray_abs_angle > 0 && ray_abs_angle < (M_PI / 2))
-	{
-		alpha = ray_abs_angle ;
-		return(CUBE_SIZE - ((int)((ray_length * cos(alpha)) - (CUBE_SIZE - (cub3d->player.x % CUBE_SIZE))) % CUBE_SIZE));
-	}	
+		return (CUBE_SIZE - ((int)((ray_length * cos(alpha))
+				- (CUBE_SIZE - (cub3d->player.x % CUBE_SIZE))) % CUBE_SIZE));
 	else if (ray_abs_angle > (3 * M_PI / 2) && ray_abs_angle < 2 * M_PI)
-	{
-		alpha = 2 * M_PI - ray_abs_angle;
-		return(CUBE_SIZE - ((int)(ray_length * cos(alpha) - (CUBE_SIZE - (cub3d->player.x % CUBE_SIZE))) % CUBE_SIZE));
-	}
+		return (CUBE_SIZE - ((int)(ray_length * cos(alpha)
+				- (CUBE_SIZE - (cub3d->player.x % CUBE_SIZE))) % CUBE_SIZE));
 	else if (ray_abs_angle > M_PI / 2 && ray_abs_angle < M_PI)
-	{
-		alpha = M_PI - ray_abs_angle;
-		return(int)((ray_length * cos(alpha)) - (cub3d->player.x % CUBE_SIZE));
-	}
+		return ((int)((ray_length * cos(alpha))
+			- (cub3d->player.x % CUBE_SIZE)));
 	else if (ray_abs_angle > M_PI && ray_abs_angle < 3 * M_PI / 2)
-	{
-		alpha = ray_abs_angle - M_PI;
-		return(int)((ray_length * cos(alpha)) - (cub3d->player.x % CUBE_SIZE));
-	}
+		return ((int)((ray_length * cos(alpha))
+			- (cub3d->player.x % CUBE_SIZE)));
 	else
 		return (ft_particular_angle(cub3d->player.x, ray_abs_angle));
+}
+
+double static	ft_calc_alpha_y(double ray_abs_angle)
+{
+	if (ray_abs_angle > M_PI / 2 && ray_abs_angle < (M_PI))
+		return (ray_abs_angle - M_PI);
+	else if (ray_abs_angle > (0) && ray_abs_angle < M_PI / 2)
+		return (ray_abs_angle);
+	else if (ray_abs_angle > M_PI && ray_abs_angle < 3 * M_PI / 2)
+		return (M_PI - ray_abs_angle);
+	else if (ray_abs_angle > 3 * M_PI / 2 && ray_abs_angle < 2 * M_PI)
+		return (2 * M_PI - ray_abs_angle);
+	else
+		return (ray_abs_angle);
 }
 
 int	ft_texture_y_offset(t_cub3d *cub3d, double ray_abs_angle, double ray_length)
 {
 	double	alpha;
+
+	alpha = ft_calc_alpha_y(ray_abs_angle);
 	if (ray_abs_angle > M_PI / 2 && ray_abs_angle < (M_PI))
-	{
-		alpha = ray_abs_angle - M_PI;
-		return((int)((ray_length * sin(alpha)) - (cub3d->player.y % CUBE_SIZE)) % CUBE_SIZE);// droite fond
-	}	
+		return ((int)((ray_length * sin(alpha))
+			- (cub3d->player.y % CUBE_SIZE)) % CUBE_SIZE);
 	else if (ray_abs_angle > (0) && ray_abs_angle < M_PI / 2)
-	{
-		alpha = ray_abs_angle;
-		return(CUBE_SIZE - ((int)(ray_length * sin(alpha) - (CUBE_SIZE - (cub3d->player.y % CUBE_SIZE))) % CUBE_SIZE) );// mur gauche devant
-	}
+		return (CUBE_SIZE - ((int)(ray_length * sin(alpha)
+				- (CUBE_SIZE - (cub3d->player.y % CUBE_SIZE))) % CUBE_SIZE));
 	else if (ray_abs_angle > M_PI && ray_abs_angle < 3 * M_PI / 2)
-	{
-		alpha = M_PI - ray_abs_angle;
-		return(CUBE_SIZE - ((int)((ray_length * sin(alpha)) - (CUBE_SIZE - (cub3d->player.y % CUBE_SIZE))) % CUBE_SIZE) );// droite devant
-	}
+		return (CUBE_SIZE - ((int)((ray_length * sin(alpha))
+				- (CUBE_SIZE - (cub3d->player.y % CUBE_SIZE))) % CUBE_SIZE));
 	else if (ray_abs_angle > 3 * M_PI / 2 && ray_abs_angle < 2 * M_PI)
-	{
-		alpha = 2 * M_PI - ray_abs_angle;
-		return((int)((ray_length * sin(alpha)) - (cub3d->player.y % CUBE_SIZE)) % CUBE_SIZE);// mur gauche derriere
-	}
+		return ((int)((ray_length * sin(alpha))
+			- (cub3d->player.y % CUBE_SIZE)) % CUBE_SIZE);
 	else
 		return (ft_particular_angle(cub3d->player.y, ray_abs_angle));
 }
